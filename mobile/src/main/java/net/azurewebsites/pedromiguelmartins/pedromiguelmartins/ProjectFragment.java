f68280aa2c85e8dcae7d48092ea96c79aa9f53a4
Pedro Martins <mourao.martins@gmail.com>
2016-09-07 17:43:25 +0100

    Update Fragmets List Project android

--- mobile/src/main/java/net/azurewebsites/pedromiguelmartins/pedromiguelmartins/ProjectFragment.java
+++ mobile/src/main/java/net/azurewebsites/pedromiguelmartins/pedromiguelmartins/ProjectFragment.java
@@ -2,6 +2,7 @@ package net.azurewebsites.pedromiguelmartins.pedromiguelmartins;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,6 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import net.azurewebsites.pedromiguelmartins.pedromiguelmartins.project.ProjectContent;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A fragment representing a list of Items.
@@ -43,6 +51,50 @@ public class ProjectFragment extends Fragment {
         return fragment;
     }
 
+    private static List<ProjectContent.ProjectItem> loadXmlFromXML(String urlString, Context context) throws XmlPullParserException, IOException {
+        AssetManager assetManager = context.getResources().getAssets();
+        InputStream stream = null;
+        // Instantiate the parser
+        ProjectXmlParser stackOverflowXmlParser = new ProjectXmlParser();
+        List<ProjectXmlParser.Entry> entries = null;
+        String title = null;
+        String url = null;
+        String summary = null;
+
+
+        try {
+            stream = assetManager.open(urlString);
+            entries = stackOverflowXmlParser.parse(stream);
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        // StackOverflowXmlParser returns a List (called "entries") of Entry objects.
+        // Each Entry object represents a single post in the XML feed.
+        // This section processes the entries list to combine each entry with HTML markup.
+        // Each entry is displayed in the UI as a link that optionally includes
+        // a text summary.
+        List<ProjectContent.ProjectItem> ITEMS = new ArrayList<ProjectContent.ProjectItem>();
+        for (ProjectXmlParser.Entry entry : entries) {
+
+            // If the user set the preference to include summary text,
+            // adds it to the display.
+            Integer value = Integer.parseInt(entry.link);
+            if (value == null)
+                value = 14;
+            Integer res = Utils.GetResumeListImages()[value];
+            if (res == null)
+                res = 14;
+            ITEMS.add(new ProjectContent.ProjectItem(entry.title, entry.content, entry.details, entry.summary, res));
+        }
+
+        return ITEMS;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,11 +118,19 @@ public class ProjectFragment extends Fragment {
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
+
+            try {
+                recyclerView.setAdapter(new MyProjectRecyclerViewAdapter(loadXmlFromXML(getResources().getString(R.string.URL_RESUME), context), mListener));
+                return view;
+            } catch (XmlPullParserException e) {
+                //e.printStackTrace();
+            } catch (IOException e) {
+                // e.printStackTrace();
+            }
             recyclerView.setAdapter(new MyProjectRecyclerViewAdapter(ProjectContent.ITEMS, mListener));
         }
         return view;
     }
-
 
     @Override
     public void onAttach(Context context) {
